a0a9657caba3c68bd40e9bf6dc7b53d8e3be3fec
lucasmrodrigues-dev <lucasmaraujo2001@gmail.com>
2026-08-28 16:14:07 -0300

    Add files via upload
    Programa em Python que recebe dois números inteiros e exibe a soma deles.

---
+++ Exercicio1.py
@@ -0,0 +1,4 @@
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+a = int(input("Digite o primeiro número:")) 
+b = int(input("Digite o segundo número:")) 
+print(a + b)
