cce6a21149d6dca2cfb1fa0ea6a1bd494efc03d3
lucasmrodrigues-dev <lucasmaraujo2001@gmail.com>
2026-08-28 16:36:53 -0300

    Add files via upload
    Programa em Python que converte uma temperatura de Celsius (°C) para Fahrenheit (°F).

---
+++ Exercicio6.py
@@ -0,0 +1,6 @@
+temperatura_celsius = float(input("Digite a temperatura em °C: "))
+
+temperatura_fahrenheit = (9 * temperatura_celsius / 5) + 32
+
+print("\n--- Resultado ---")
+print(f"{temperatura_celsius:.2f}°C equivale a {temperatura_fahrenheit:.2f}°F")
