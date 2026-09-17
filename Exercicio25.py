b6e4762b3d21eba713041d41d342757b9d10a01e
Lucas Manoel <lucasmaraujo2001@gmail.com>
2026-09-17 08:52:02 -0300

    Add files via upload
    Neste exercício, foi criado um dicionário em Python contendo o nome, o valor e a quantidade de um produto. Em seguida, o programa acessa os valores do dicionário e realiza a multiplicação do preço pela quantidade para calcular o valor total.

---
+++ Exercicio25.py
@@ -0,0 +1,12 @@
+#Crie um dicionário chamado aluno contendo: nome, idade, curso , cidade
+
+produto = {
+
+    "nome": "Varios",
+    "ano": 50,
+    "quantidade": 3
+}
+
+total = produto["ano"] * produto["quantidade"]
+
+print(total)
